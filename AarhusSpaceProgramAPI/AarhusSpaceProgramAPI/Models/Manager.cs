8368f912becb6964ac96d126a4fd7591b64d7c12
Mikkel Kristiansen <202301563@post.au.dk>
2026-03-22 12:28:24 +0100

    added attributes to Manager.cs

--- AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Manager.cs
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Manager.cs
@@ -2,5 +2,8 @@
 
 public class Manager
 {
-    
+    public int ManagerId { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public DateTime HireDate { get; set; }
 }
