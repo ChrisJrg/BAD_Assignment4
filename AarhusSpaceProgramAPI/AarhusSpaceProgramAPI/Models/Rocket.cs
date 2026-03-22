edd66aa3632f037e02e1071a3712db60a9d7f1ea
Mikkel Kristiansen <202301563@post.au.dk>
2026-03-22 12:33:41 +0100

    added attributes for Rocket.cs

--- AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Rocket.cs
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Rocket.cs
@@ -2,5 +2,11 @@
 
 public class Rocket
 {
-    
+    public int RocketId { get; set; }
+    public string Model { get; set; }
+    public double Weight { get; set; }
+    public int CrewCapacity { get; set; }
+    public int Stages { get; set; }
+    public double FuelCapacity { get; set; }
+    public double PayloadCapacity { get; set; }
 }
