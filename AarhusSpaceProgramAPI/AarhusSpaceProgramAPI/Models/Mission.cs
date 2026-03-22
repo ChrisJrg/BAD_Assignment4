06260b0c01b3d1cd5361995b60a673014c046a56
Mikkel Kristiansen <202301563@post.au.dk>
2026-03-22 12:31:01 +0100

    added attributes to Mission.cs

--- AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Mission.cs
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/Mission.cs
@@ -2,5 +2,10 @@
 
 public class Mission
 {
-    
+    public int MissionId { get; set; }
+    public string MissionName { get; set; }
+    public DateTime LaunchDate { get; set; }
+    public double Duration { get; set; }
+    public string Status { get; set; }
+    public string Type { get; set; }
 }
