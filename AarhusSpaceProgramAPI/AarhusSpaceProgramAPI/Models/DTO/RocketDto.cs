0221a6ded831cc0c642a2ec35732a18b906a7a57
Christopher F. Jørgensen <202300791@post.au.dk>
2026-03-24 21:15:44 +0100

    added DTO's for all but celestial bodies

---
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/DTO/RocketDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AarhusSpaceProgramAPI.Models;
+
+public class RocketDto
+{
+    public int RocketId { get; set; }
+    public string Model { get; set; }
+    public double Weight { get; set; }
+    public int CrewCapacity { get; set; }
+    public int Stages { get; set; }
+    public double FuelCapacity { get; set; }
+    public double PayloadCapacity { get; set; }
+    
+    public ICollection<Mission> Missions { get; set; } = new List<Mission>();
+}
---
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/DTO/ScientistDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AarhusSpaceProgramAPI.Models;
+
+public class ScientistDto
+{
+    public int ScientistId { get; set; }
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+    public string Title {get; set;}
+    public string Specialty { get; set; }
+    
+    public ICollection<Mission> Missions { get; set; } =  new List<Mission>();
+}
