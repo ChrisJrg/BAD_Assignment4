6d97be83c49f8c59e268c5efad14ca0b0fb1ba27
Mikkel Kristiansen <202301563@post.au.dk>
2026-03-24 23:40:22 +0100

    added Controller with CRUD operations for ManagerController

---
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Controllers/ManagerController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AarhusSpaceProgramAPI.Data;
+using AarhusSpaceProgramAPI.Models;
+
+namespace AarhusSpaceProgramAPI.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class ManagerController : ControllerBase
+{
+    private readonly  ApplicationDbContext _context;
+
+    public ManagerController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ManagerDto>>> GetManagers()
+    {
+        var manager = await _context.Managers
+            .Select(m => new ManagerDto
+            {
+                ManagerId = m.ManagerId,
+                Name = m.Name,
+                Department =  m.Department,
+                HireDate = m.HireDate,
+            }).ToListAsync();
+        
+        return Ok(manager);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ManagerDto>> CreateManager(ManagerDto dto)
+    {
+        var manager = new Manager
+        {
+            Name = dto.Name,
+            Department = dto.Department,
+            HireDate = dto.HireDate,
+        };
+        
+        _context.Managers.Add(manager);
+        await _context.SaveChangesAsync();
+
+        var resultDto = new ManagerDto
+        {
+            ManagerId = manager.ManagerId,
+            Name = manager.Name,
+            Department = manager.Department,
+            HireDate = manager.HireDate,
+        };
+        
+        return CreatedAtAction(nameof(GetManagers), new { id = manager.ManagerId }, resultDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateManager(int id, ManagerDto dto)
+    {
+        var  manager = await _context.Managers.FindAsync(id);
+        if (manager == null)
+        {
+            return NotFound();
+        }
+        manager.Name = dto.Name;
+        manager.Department = dto.Department;
+        manager.HireDate = dto.HireDate;
+        
+        _context.Entry(manager).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteManager(int id)
+    {
+        var manager = await _context.Managers.FindAsync(id);
+        if (manager == null)
+        {
+            return NotFound();
+        }
+
+        _context.Managers.Remove(manager);
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+    
+}
