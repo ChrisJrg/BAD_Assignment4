bcf9a0059d6f96aeae9145eac942ad1ceaa565ad
Mikkel Kristiansen <202301563@post.au.dk>
2026-03-25 18:37:11 +0100

    added query endpoints for AstronautExperienceDto.cs and MissionOverviewDto.cs

--- AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/DTO/AstronautExperienceDto.cs
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/DTO/AstronautExperienceDto.cs
@@ -2,5 +2,8 @@
 
 public class AstronautExperienceDto
 {
-    
+    public int AstronautId  { get; set; }
+    public string Name  { get; set; }
+    public string Rank { get; set; }
+    public double EXPInSpace { get; set; }
 }
---
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Models/DTO/MissionOverviewDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AarhusSpaceProgramAPI.Models;
+
+public class MissionOverviewDto
+{
+    public string MissionName { get; set; }
+    public DateTime LaunchDate { get; set; }
+    public string ManagerName { get; set; }
+    public string RocketModel { get; set; }
+    public string LaunchPadLocation { get; set; }
+    public string TargetBodyName { get; set; }
+}
