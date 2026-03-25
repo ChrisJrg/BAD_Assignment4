316eebe388952f7e748c6b4435ba6e46a3f5a519
Christopher F. Jørgensen <202300791@post.au.dk>
2026-03-25 18:13:23 +0100

    new migrationsitwasabitch

---
+++ AarhusSpaceProgramAPI/AarhusSpaceProgramAPI/Migrations/20260325171005_TypoInMissionModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AarhusSpaceProgramAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypoInMissionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
